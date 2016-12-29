6cd823e509471c982a031ba8156ef1695d49172f
nyyynyyy <nyyynyyy@naver.com>
2016-12-29 16:26:48 +0900

    Tesst

--- Assets/script1.cs
+++ Assets/script1.cs
@@ -7,6 +7,7 @@ public class script1 : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("나는 자바도둑");
+        Debug.Log("나는 포켓몬마스터");
 	}
 	
 	// Update is called once per frame
