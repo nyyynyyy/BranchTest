ef4dc9fde742e0ea3141973c56a12abe276b9688
nyyynyyy <nyyynyyy@naver.com>
2016-12-29 16:36:07 +0900

    PO

---
+++ Assets/script2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log("포켓몬은 갓겜이다");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
